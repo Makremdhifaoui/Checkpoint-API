1fc2e8f6576f4ae02b08ec8f65681b8474525a50
Makremdhifaoui <102286035+Makremdhifaoui@users.noreply.github.com>
2022-09-08 10:59:27 +0100

    checkpoint API

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import UserList from "./components/UserList";
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{textAlign: "center"}}>User List</h1>
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
